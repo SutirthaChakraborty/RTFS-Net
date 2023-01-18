f53f1984141978a0d6fbe2f8da0b20a7b20dcad5
安熙皓 <spkgyk@gmail.com>
2023-01-18 16:15:25 +0800

    python version update

--- src/utils/parser_utils.py
+++ src/utils/parser_utils.py
@@ -5,6 +5,7 @@
 # LastEditTime: 2021-06-20 00:48:50
 ###
 
+import sys
 import argparse
 
 
@@ -143,12 +144,12 @@ def parse_args_as_dict(parser, return_plain_args=False, args=None):
     for group in parser._action_groups:
         group_dict = {a.dest: getattr(args, a.dest, None) for a in group._group_actions}
         args_dic[group.title] = group_dict
-    if args_dic.get("optional arguments", False):
+    if sys.version_info.minor == 10:
+        args_dic["main_args"] = args_dic["positional arguments"]
+        del args_dic["positional arguments"]
+    else:
         args_dic["main_args"] = args_dic["optional arguments"]
         del args_dic["optional arguments"]
-    else:
-        args_dic["main_args"] = args_dic["options"]
-        del args_dic["options"]
     if return_plain_args:
         return args_dic, args
     return args_dic
