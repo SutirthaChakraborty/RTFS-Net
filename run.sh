efce254d5908d84cf9fde0bd85e81139015b72b1
spkgyk <spkgyk@gmail.com>
2023-02-02 18:06:51 +0800

    update run to work

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-name="lrs2_conf_small_frcnn"
+name="lrs2_conf_large_frcnn"
 config=$name".yml"
+exp="ctcnet_large_frcnn"
 echo "Stage 1: Training: python train.py --conf-dir config/"$config
 python train.py --conf-dir config/$config
-echo "Stage 2: Evaluation: python eval.py --conf-dir ../experiments/audio-visual/"$name"/conf.yml --test-dir data-preprocess/LRS2/tt"
-python eval.py --conf-dir ../experiments/audio-visual/$name/conf.yml --test-dir data-preprocess/LRS2/tt
+echo "Stage 2: Evaluation: python eval.py --conf-dir ../experiments/audio-visual/"$exp"/conf.yml --test-dir data-preprocess/LRS2/tt"
+python eval.py --conf-dir ../experiments/audio-visual/$exp/conf.yml --test-dir data-preprocess/LRS2/tt
