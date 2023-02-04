145a18a9a87912b9dfa41cdd00a8236a49bd1ff3
spkgyk <spkgyk@gmail.com>
2023-02-05 05:16:21 +0800

    update conf

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-name="lrs2_conf_small_tdanet"
+name="lrs2_conf_large_frcnn"
 config=$name".yml"
-exp="ctcnet_small_tdanet_vidbatchnorm"
+exp="ctcnet_large_frcnn"
 echo "Stage 1: Training: python train.py --conf-dir config/"$config
 python train.py --conf-dir config/$config
 echo "Stage 2: Evaluation: python eval.py --conf-dir ../experiments/audio-visual/"$exp"/conf.yml --test-dir data-preprocess/LRS2/tt"
