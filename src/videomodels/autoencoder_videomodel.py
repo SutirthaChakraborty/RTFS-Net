dc8a6e2d738a816ff04c132346c4a4e5953b15c0
spkgyk <spkgyk@gmail.com>
2023-03-14 15:27:58 +0800

    update config, add loading function

--- src/videomodels/autoencoder_videomodel.py
+++ src/videomodels/autoencoder_videomodel.py
@@ -11,20 +11,27 @@ class AEVideoModel(nn.Module):
         self.num_layers = num_layers
         self.pretrain = pretrain
 
-        self.encoder = EncoderAE(in_channels=self.in_channels, base_channels=self.base_channels, num_layers=self.num_layers)
-        self.out_channels = self.encoder.out_chan
+        self.encoder = EncoderAE(
+            in_channels=self.in_channels,
+            base_channels=self.base_channels,
+            num_layers=self.num_layers,
+        )
 
-        self.encoder.load_state_dict(torch.load(pretrain))
-        self.encoder.eval()
+        self.out_channels = self.encoder.out_chan
+        if self.pretrain:
+            self.init_from(self.pretrain)
 
     def forward(self, x: torch.Tensor):
         batch, chan, frames, h, w = x.size()
-        assert chan == 1
 
-        x = x.view(batch * frames, 1, h, w)  # B, F, H, W -> B*F, 1, H, W
+        x = x.transpose(1, 2).contiguous().view(batch * frames, chan, h, w)  # B, F, H, W -> B*F, 1, H, W
 
         z = self.encoder.forward(x)  # B*F, 1, H, W -> B*F, C, H', W'
 
         z = z.view(batch, frames, -1)  # B*F, C, H', W' -> B, F, C*H'*W'
 
         return z
+
+    def init_from(self, pretrain):
+        self.encoder.load_state_dict(torch.load(pretrain, map_location="cpu"))
+        self.encoder.eval()
