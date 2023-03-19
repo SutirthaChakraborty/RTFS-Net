e9a86a3bbc5bf557d89fbeac490d81038c48744a
spkgyk <spkgyk@gmail.com>
2023-03-20 04:19:50 +0800

    wrong attention layer

--- src/models/layers/attention.py
+++ src/models/layers/attention.py
@@ -137,60 +137,6 @@ class GlobalAttention(nn.Module):
         return x
 
 
-# class GlobalAttention2D(nn.Module):
-#     def __init__(
-#         self,
-#         in_chan: int,
-#         hid_chan: int = None,
-#         kernel_size: int = 5,
-#         n_head: int = 8,
-#         dropout: float = 0.1,
-#         drop_path: float = 0.1,
-#         *args,
-#         **kwargs,
-#     ):
-#         super(GlobalAttention2D, self).__init__()
-#         self.in_chan = in_chan
-#         self.hid_chan = hid_chan if hid_chan is not None else 2 * self.in_chan
-#         self.kernel_size = kernel_size
-#         self.n_head = n_head
-#         self.dropout = dropout
-#         self.drop_path = drop_path
-
-#         if self.n_head == -1:
-#             self.mhsa = ConvMod(self.in_chan)
-#         else:
-#             self.mhsa_height = MultiHeadSelfAttention(self.in_chan, self.n_head, self.dropout)
-#             self.mhsa_width = MultiHeadSelfAttention(self.in_chan, self.n_head, self.dropout)
-
-#         if self.kernel_size == -1:
-#             self.ffn = RNNProjection(self.in_chan, self.hid_chan, dropout=dropout, bidirectional=True)
-#         else:
-#             self.ffn = FeedForwardNetwork(self.in_chan, self.hid_chan, self.kernel_size, dropout=self.dropout, is2d=True)
-#         self.drop_path_layer = DropPath(self.drop_path) if self.drop_path > 0.0 else nn.Identity()
-
-#     def forward(self, x: torch.Tensor):
-#         B, C, H, W = x.size()
-
-#         if self.n_head == -1:
-#             x = x + self.drop_path_layer(self.mhsa(x))
-#             x = x + self.drop_path_layer(self.ffn(x))
-#         else:
-#             h_input = x.permute(0, 3, 1, 2).contiguous().view(B * W, C, H)
-#             h_output = self.mhsa_height.forward(h_input).view(B, W, C, H)
-#             h_output = h_output.permute(0, 2, 3, 1).contiguous()
-#             x = x + self.drop_path_layer(h_output)
-#             x = x + self.drop_path_layer(self.ffn(x))
-
-#             w_input = x.permute(0, 2, 1, 3).contiguous().view(B * H, C, W)
-#             w_output = self.mhsa_width.forward(w_input).view(B, H, C, W)
-#             w_output = w_output.permute(0, 2, 1, 3).contiguous()
-#             x = x + self.drop_path_layer(w_output)
-#             x = x + self.drop_path_layer(self.ffn(x))
-
-#         return x
-
-
 class GlobalAttention2D(nn.Module):
     def __init__(
         self,
@@ -217,7 +163,10 @@ class GlobalAttention2D(nn.Module):
             self.mhsa_height = MultiHeadSelfAttention(self.in_chan, self.n_head, self.dropout)
             self.mhsa_width = MultiHeadSelfAttention(self.in_chan, self.n_head, self.dropout)
 
-        self.ffn = RNNProjection(self.in_chan, self.hid_chan, dropout=dropout, bidirectional=True)
+        if self.kernel_size == -1:
+            self.ffn = RNNProjection(self.in_chan, self.hid_chan, dropout=dropout, bidirectional=True)
+        else:
+            self.ffn = FeedForwardNetwork(self.in_chan, self.hid_chan, self.kernel_size, dropout=self.dropout, is2d=True)
         self.drop_path_layer = DropPath(self.drop_path) if self.drop_path > 0.0 else nn.Identity()
 
     def forward(self, x: torch.Tensor):
@@ -231,18 +180,69 @@ class GlobalAttention2D(nn.Module):
             h_output = self.mhsa_height.forward(h_input).view(B, W, C, H)
             h_output = h_output.permute(0, 2, 3, 1).contiguous()
             x = x + self.drop_path_layer(h_output)
-            h_input = x.permute(0, 3, 1, 2).contiguous().view(B * W, C, H)
-            h_output = self.ffn.forward(h_input).view(B, W, C, H)
-            h_output = h_output.permute(0, 2, 3, 1).contiguous()
-            x = x + self.drop_path_layer(h_output)
+            x = x + self.drop_path_layer(self.ffn(x))
 
             w_input = x.permute(0, 2, 1, 3).contiguous().view(B * H, C, W)
             w_output = self.mhsa_width.forward(w_input).view(B, H, C, W)
             w_output = w_output.permute(0, 2, 1, 3).contiguous()
             x = x + self.drop_path_layer(w_output)
-            w_input = x.permute(0, 2, 1, 3).contiguous().view(B * H, C, W)
-            w_output = self.ffn.forward(w_input).view(B, H, C, W)
-            w_output = w_output.permute(0, 2, 1, 3).contiguous()
-            x = x + self.drop_path_layer(w_output)
+            x = x + self.drop_path_layer(self.ffn(x))
 
         return x
+
+
+# class GlobalAttention2D(nn.Module):
+#     def __init__(
+#         self,
+#         in_chan: int,
+#         hid_chan: int = None,
+#         kernel_size: int = 5,
+#         n_head: int = 8,
+#         dropout: float = 0.1,
+#         drop_path: float = 0.1,
+#         *args,
+#         **kwargs,
+#     ):
+#         super(GlobalAttention2D, self).__init__()
+#         self.in_chan = in_chan
+#         self.hid_chan = hid_chan if hid_chan is not None else 2 * self.in_chan
+#         self.kernel_size = kernel_size
+#         self.n_head = n_head
+#         self.dropout = dropout
+#         self.drop_path = drop_path
+
+#         if self.n_head == -1:
+#             self.mhsa = ConvMod(self.in_chan)
+#         else:
+#             self.mhsa_height = MultiHeadSelfAttention(self.in_chan, self.n_head, self.dropout)
+#             self.mhsa_width = MultiHeadSelfAttention(self.in_chan, self.n_head, self.dropout)
+
+#         self.ffn = RNNProjection(self.in_chan, self.hid_chan, dropout=dropout, bidirectional=True)
+#         self.drop_path_layer = DropPath(self.drop_path) if self.drop_path > 0.0 else nn.Identity()
+
+#     def forward(self, x: torch.Tensor):
+#         B, C, H, W = x.size()
+
+#         if self.n_head == -1:
+#             x = x + self.drop_path_layer(self.mhsa(x))
+#             x = x + self.drop_path_layer(self.ffn(x))
+#         else:
+#             h_input = x.permute(0, 3, 1, 2).contiguous().view(B * W, C, H)
+#             h_output = self.mhsa_height.forward(h_input).view(B, W, C, H)
+#             h_output = h_output.permute(0, 2, 3, 1).contiguous()
+#             x = x + self.drop_path_layer(h_output)
+#             h_input = x.permute(0, 3, 1, 2).contiguous().view(B * W, C, H)
+#             h_output = self.ffn.forward(h_input).view(B, W, C, H)
+#             h_output = h_output.permute(0, 2, 3, 1).contiguous()
+#             x = x + self.drop_path_layer(h_output)
+
+#             w_input = x.permute(0, 2, 1, 3).contiguous().view(B * H, C, W)
+#             w_output = self.mhsa_width.forward(w_input).view(B, H, C, W)
+#             w_output = w_output.permute(0, 2, 1, 3).contiguous()
+#             x = x + self.drop_path_layer(w_output)
+#             w_input = x.permute(0, 2, 1, 3).contiguous().view(B * H, C, W)
+#             w_output = self.ffn.forward(w_input).view(B, H, C, W)
+#             w_output = w_output.permute(0, 2, 1, 3).contiguous()
+#             x = x + self.drop_path_layer(w_output)
+
+#         return x
