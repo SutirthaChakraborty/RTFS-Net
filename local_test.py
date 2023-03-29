b239ff6dc86e828e202ad3153892e6e27407af0d
安熙皓 <spkgyk@gmail.com>
2023-03-29 11:31:31 +0800

    asdf

--- local_test.py
+++ local_test.py
@@ -156,7 +156,7 @@ def main(conf, model=CTCNet, epochs=1):
 if __name__ == "__main__":
     t0 = time()
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--conf-dir", default="config/lrs2_conf_small_tdanet2d_ae_2d_mini.yml")
+    parser.add_argument("-c", "--conf-dir", default="config/lrs2_conf_small_tdanet2d_ae_2d_bsrnn.yml")
     parser.add_argument("-n", "--name", default=None, help="Experiment name")
     parser.add_argument("--nodes", type=int, default=1, help="#node")
 
