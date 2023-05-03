a6c5d18837746cc2bad490a5c890ae81a51a581b
spkgyk <spkgyk@gmail.com>
2023-05-03 18:45:34 +0800

    asf

--- src/models/CTCNet/refinement_module.py
+++ src/models/CTCNet/refinement_module.py
@@ -24,6 +24,7 @@ class RefinementModule(nn.Module):
 
         self.fusion_repeats = self.video_params.get("repeats", 0)
         self.audio_repeats = self.audio_params["repeats"] - self.fusion_repeats
+        self.is2d = self.audio_params.get("is2d", False)
 
         self.audio_net = separators.get(self.audio_params.get("audio_net", None))(**self.audio_params, in_chan=self.audio_bn_chan)
         self.video_net = separators.get(self.video_params.get("video_net", None))(**self.video_params, in_chan=self.video_bn_chan)
@@ -36,6 +37,10 @@ class RefinementModule(nn.Module):
         )
 
     def forward(self, audio: torch.Tensor, video: torch.Tensor):
+        return self.forward2d(audio, video) if self.is2d else self.forward1d(audio, video)
+
+    def forward1d(self, audio: torch.Tensor, video: torch.Tensor):
+        print('1d')
         audio_residual = audio
         video_residual = video
 
@@ -43,8 +48,10 @@ class RefinementModule(nn.Module):
         for i in range(self.fusion_repeats):
             audio = self.audio_net.get_concat_block(i)(audio + audio_residual) if i > 0 else audio
             video = self.video_net.get_concat_block(i)(video + video_residual) if i > 0 else video
+
             audio = self.audio_net.get_block(i)(audio)
             video = self.video_net.get_block(i)(video)
+
             audio, video = self.crossmodal_fusion.get_fusion_block(i)(audio, video)
 
         # further refinement
@@ -52,7 +59,33 @@ class RefinementModule(nn.Module):
             i = j + self.fusion_repeats
 
             audio = self.audio_net.get_concat_block(i)(audio + audio_residual) if i > 0 else audio
+
             audio = self.audio_net.get_block(i)(audio)
+
+        return audio
+
+    def forward2d(self, audio: torch.Tensor, video: torch.Tensor):
+        print('2d')
+        audio_residual = audio
+        video_residual = video
+
+        # cross modal fusion
+        for i in range(self.fusion_repeats):
+            audio = audio + audio_residual if i > 0 else audio
+            video = video + video_residual if i > 0 else video
+
+            audio = self.audio_net.get_block(i)(self.audio_net.get_concat_block(i)(audio))
+            video = self.video_net.get_block(i)(self.video_net.get_concat_block(i)(video))
+
+            audio, video = self.crossmodal_fusion.get_fusion_block(i)(audio, video)
+
+        # further refinement
+        for j in range(self.audio_repeats):
+            i = j + self.fusion_repeats
+
+            audio = audio + audio_residual if i > 0 else audio
+
+            audio = self.audio_net.get_block(i)(self.audio_net.get_concat_block(i)(audio))
 
         return audio
 
