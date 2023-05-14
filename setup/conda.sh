66224d61226040014ae5c704fc6916fcf2e9962a
anxihao <spkgyk@gmail.com>
2023-05-15 02:14:13 +0800

    update conda. Run with source

--- setup/conda.sh
+++ setup/conda.sh
@@ -1,8 +1,8 @@
+conda activate base
 conda remove -n av --all --yes
-source ~/.bashrc
-conda create -n av pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia --yes
-source ~/.bashrc
+conda create -n av numpy --yes
 conda activate av
-python -m pip3 install --upgrade pip3
+pip3 install --upgrade pip
 pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 --no-cache-dir 
 pip3 install -r requirements.txt --no-cache-dir 
