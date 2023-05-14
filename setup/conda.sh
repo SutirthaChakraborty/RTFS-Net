30bbc2e8ca948290290de06ce3f6fc0801e4dbfe
anxihao <spkgyk@gmail.com>
2023-05-15 01:31:30 +0800

    asdf

--- setup/conda.sh
+++ setup/conda.sh
@@ -1,5 +1,7 @@
 conda remove -n av --all --yes
+source ~/.bashrc
 conda create -n av pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia --yes
+source ~/.bashrc
 conda activate av
 python -m pip3 install --upgrade pip3
 pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
