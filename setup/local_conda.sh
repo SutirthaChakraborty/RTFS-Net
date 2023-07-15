ec737f00d9ce13145e91b34acaa96d95e5102d95
spkgyk-ubuntu <spkgyk@gmail.com>
2023-07-16 02:31:58 +0800

    collecting ks back to 1

--- setup/local_conda.sh
+++ setup/local_conda.sh
@@ -1,9 +1,9 @@
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 conda activate base
 conda remove -n av --all --yes
 conda create -n av numpy --yes
 conda activate av
 pip install --upgrade pip
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install https://github.com/vBaiCai/python-pesq/archive/master.zip
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 
 pip install -r requirements.txt
