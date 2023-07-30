8d4cfe9d5a4212aebf87b73777647a994991698f
spkgyk <spkgyk@gmail.com>
2023-07-31 01:30:38 +0800

    configs for training

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 name=(
-        lrs2_tdavnet_mini_512
-        lrs2_tdavnet_mini_256
-        lrs2_tdavnet_mini_128
-        lrs2_tdavnet_mini_64
         lrs2_tdavnet_mini
+        lrs2_tdavnet_mini_injfus_1
+        lrs2_tdavnet_mini_injfus_5
 )
 for n in "${name[@]}"
 do
@@ -12,11 +10,9 @@ do
     python train.py --conf-dir config/$config
 done
 exp=(
-        avnet_mini/tdavnet/22_07_23
-        avnet_mini/tdavnet/22_07_23_64
-        avnet_mini/tdavnet/22_07_23_128
-        avnet_mini/tdavnet/22_07_23_256
-        avnet_mini/tdavnet/22_07_23_512
+        avnet_mini/tdavnet/31_07_23
+        avnet_mini/tdavnet/31_07_23_InjFus_1
+        avnet_mini/tdavnet/31_07_23_InjFus_5
 )
 for e in "${exp[@]}"
 do
