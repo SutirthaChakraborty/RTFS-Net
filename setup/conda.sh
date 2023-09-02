9304e5337bd638f2a474b46de752963abd74cfe6
anxihao <spkgyk@gmail.com>
2023-09-02 20:56:14 +0800

    install updates

--- setup/conda.sh
+++ setup/conda.sh
@@ -1,7 +1,4 @@
 conda activate base
 conda remove -n av --all --yes
-conda create -n av numpy pip pytorch=2.0.0 torchvision=0.15.0 torchaudio=2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia --yes
+conda env create -f setup/requirements.yml
 conda activate av
-pip install --upgrade pip
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple 
-pip install -r setup/requirements.txt --no-cache-dir
