1e242530f206552d72cb31a83e045afee069f638
spkgyk <spkgyk@gmail.com>
2023-09-10 15:21:23 +0800

    runfile update

--- run.sh
+++ run.sh
@@ -8,10 +8,44 @@ do
     python train.py --conf-dir config/$config
 done
 exp=(
-        avnet_mini/tdavnet/07_09_23_attn_fusion
+        RTFS-Net/LRS2/4_layers_10-09-23
 )
 for e in "${exp[@]}"
 do
     echo "Stage 2: Evaluation: python eval.py --conf-dir ../experiments/audio-visual/"$e"/conf.yml --test-dir data-preprocess/LRS2/tt"
     python eval.py --conf-dir ../experiments/audio-visual/$e/conf.yml --test-dir data-preprocess/LRS2/tt
+done
+name=(
+        lrs3_tdavnet_mini_ATTNfusion
+)
+for n in "${name[@]}"
+do
+    config=$n".yml"
+    echo "Stage 1: Training: python train.py --conf-dir config/"$config
+    python train.py --conf-dir config/$config
+done
+exp=(
+        RTFS-Net/LRS3/4_layers_10-09-23
+)
+for e in "${exp[@]}"
+do
+    echo "Stage 2: Evaluation: python eval.py --conf-dir ../experiments/audio-visual/"$e"/conf.yml --test-dir data-preprocess/LRS2/tt"
+    python eval.py --conf-dir ../experiments/audio-visual/$e/conf.yml --test-dir data-preprocess/LRS3/tt
+done
+name=(
+        voxceleb2_tdavnet_mini_ATTNfusion
+)
+for n in "${name[@]}"
+do
+    config=$n".yml"
+    echo "Stage 1: Training: python train.py --conf-dir config/"$config
+    python train.py --conf-dir config/$config
+done
+exp=(
+        RTFS-Net/VOX2/4_layers_10-09-23
+)
+for e in "${exp[@]}"
+do
+    echo "Stage 2: Evaluation: python eval.py --conf-dir ../experiments/audio-visual/"$e"/conf.yml --test-dir data-preprocess/LRS2/tt"
+    python eval.py --conf-dir ../experiments/audio-visual/$e/conf.yml --test-dir data-preprocess/LRS3/tt
 done
